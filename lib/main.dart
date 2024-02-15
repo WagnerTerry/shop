3a5f8997f3f8a1c7bd6ea8f9cc7992e22987743e
Wagner Terry <goncalveswagner15@gmail.com>
2024-02-15 20:25:35 -0300

    feat: iniciando navegação para tela detalhe do produto

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +20,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Lato'),
       home: ProductsOverviewPage(),
       debugShowCheckedModeBanner: false,
+      routes: {AppRoutes.productDetail: (ctx) => const ProductDetailPage()},
     );
   }
 }
