3e5d54d20cc765a2fbb6a327bb65d59db2f56d1d
Wagner Terry <goncalveswagner15@gmail.com>
2024-02-20 15:54:41 -0300

    feat: navegando para tela do carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/models/product_list.dart';
+import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -30,9 +31,12 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSwatch()
                 .copyWith(primary: Colors.purple, secondary: Colors.deepOrange),
             fontFamily: 'Lato'),
-        home: ProductsOverviewPage(),
+        home: const ProductsOverviewPage(),
         debugShowCheckedModeBanner: false,
-        routes: {AppRoutes.productDetail: (ctx) => const ProductDetailPage()},
+        routes: {
+          AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+          AppRoutes.cart: (ctx) => const CartPage()
+        },
       ),
     );
   }
