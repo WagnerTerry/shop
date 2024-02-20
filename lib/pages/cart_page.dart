281b6aba4a664246ada83d39f3301a39fc04b3a3
Wagner Terry <goncalveswagner15@gmail.com>
2024-02-20 17:56:14 -0300

    feat: adicionar pedidos

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/cart_item.dart';
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/order_list.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -39,7 +40,15 @@ class CartPage extends StatelessWidget {
                     ),
                   ),
                   const Spacer(),
-                  TextButton(onPressed: () {}, child: const Text('COMPRAR'))
+                  TextButton(
+                      onPressed: () {
+                        Provider.of<OrderList>(
+                          context,
+                          listen: false,
+                        ).addOrder(cart);
+                        cart.clear();
+                      },
+                      child: const Text('COMPRAR'))
                 ],
               ),
             ),
