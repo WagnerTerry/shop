ce67825e825eed6ba2433f4d4b1e5b2711b4e348
Wagner Terry <goncalveswagner15@gmail.com>
2024-02-20 23:31:10 -0300

    feat: iniciando formulario, colocando foco no preço

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -8,11 +8,35 @@ class ProductFormPage extends StatefulWidget {
 }
 
 class _ProductFormPageState extends State<ProductFormPage> {
+  final _priceFocus = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Formulário de Produto'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Nome'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocus);
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Preço'),
+                textInputAction: TextInputAction.next,
+                focusNode: _priceFocus,
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
