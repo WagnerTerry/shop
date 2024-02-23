9587a9d07bff2e09adf844944babf4ee3bd29882
Wagner Terry <goncalveswagner15@gmail.com>
2024-02-22 23:17:30 -0300

    feat: exibindo indicador de progresso ao salvar produto

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -21,7 +21,7 @@ class ProductList with ChangeNotifier {
     return _items.length;
   }
 
-  void saveProduct(Map<String, Object> data) {
+  Future<void> saveProduct(Map<String, Object> data) {
     bool hasId = data['id'] != null;
 
     final product = Product(
@@ -32,14 +32,14 @@ class ProductList with ChangeNotifier {
         imageUrl: data['imageUrl'] as String);
 
     if (hasId) {
-      updateProduct(product);
+      return updateProduct(product);
     } else {
-      addProduct(product);
+      return addProduct(product);
     }
   }
 
-  void addProduct(Product product) {
-    http.post(Uri.parse('$_baseUrl/products.json'),
+  Future<void> addProduct(Product product) {
+    final future = http.post(Uri.parse('$_baseUrl/products.json'),
         body: jsonEncode({
           "name": product.name,
           "description": product.description,
@@ -47,17 +47,30 @@ class ProductList with ChangeNotifier {
           "imageUrl": product.imageUrl,
           "isFavorite": product.isFavorite,
         }));
-    _items.add(product);
-    notifyListeners();
+
+    return future.then<void>((response) {
+      final id = jsonDecode(response.body)['name'];
+      _items.add(Product(
+        id: id,
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        isFavorite: product.isFavorite,
+      ));
+      notifyListeners();
+    });
   }
 
-  void updateProduct(Product product) {
+  Future<void> updateProduct(Product product) {
     int index = _items.indexWhere((p) => p.id == product.id);
 
     if (index >= 0) {
       _items[index] = product;
       notifyListeners();
     }
+
+    return Future.value();
   }
 
   void removeProduct(Product product) {
