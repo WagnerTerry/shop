0b12d77cab875936bcecfa5dfd50658cd5b277fe
Wagner Terry <goncalveswagner15@gmail.com>
2024-03-04 20:36:53 -0300

    feat: armazenando os pedidos no firebase

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -5,7 +5,7 @@ import 'package:shop/models/cart.dart';
 import 'package:shop/models/order_list.dart';
 
 class CartPage extends StatelessWidget {
-  const CartPage({super.key});
+  const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -40,15 +40,7 @@ class CartPage extends StatelessWidget {
                     ),
                   ),
                   const Spacer(),
-                  TextButton(
-                      onPressed: () {
-                        Provider.of<OrderList>(
-                          context,
-                          listen: false,
-                        ).addOrder(cart);
-                        cart.clear();
-                      },
-                      child: const Text('COMPRAR'))
+                  CartButton(cart: cart)
                 ],
               ),
             ),
@@ -60,5 +52,43 @@ class CartPage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class CartButton extends StatefulWidget {
+  const CartButton({
+    super.key,
+    required this.cart,
+  });
+
+  final Cart cart;
+
+  @override
+  State<CartButton> createState() => _CartButtonState();
+}
+
+class _CartButtonState extends State<CartButton> {
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? const CircularProgressIndicator()
+        : TextButton(
+            onPressed: widget.cart.itemsCount == 0
+                ? null
+                : () async {
+                    setState(() {
+                      _isLoading = true;
+                    });
+                    await Provider.of<OrderList>(
+                      context,
+                      listen: false,
+                    ).addOrder(widget.cart);
+
+                    widget.cart.clear();
+                    setState(() => _isLoading = false);
+                  },
+            child: const Text('COMPRAR'));
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const productBaseUrl =
       'https://shop-8543e-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://shop-8543e-default-rtdb.firebaseio.com/orders';
 }
