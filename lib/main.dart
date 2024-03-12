3d87ed80e294465c891d10fd03b450c7390096d0
Wagner Terry <goncalveswagner15@gmail.com>
2024-03-12 19:07:38 -0300

    feat: realizando login com firebase e gerenciando token

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:shop/models/auth.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/models/order_list.dart';
 import 'package:shop/models/product_list.dart';
+import 'package:shop/pages/auth_or_home_page.dart';
 import 'package:shop/pages/auth_page.dart';
 import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/orders_page.dart';
@@ -46,8 +47,7 @@ class MyApp extends StatelessWidget {
         // home: const ProductsOverviewPage(),
         debugShowCheckedModeBanner: false,
         routes: {
-          AppRoutes.auth: (ctx) => const AuthPage(),
-          AppRoutes.home: (ctx) => const ProductsOverviewPage(),
+          AppRoutes.authOrHome: (ctx) => const AuthOrHomePage(),
           AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
           AppRoutes.cart: (ctx) => const CartPage(),
           AppRoutes.orders: (ctx) => const OrdersPage(),
--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -6,6 +6,28 @@ import 'package:shop/constants.dart';
 import 'package:shop/exceptions/auth_exception.dart';
 
 class Auth with ChangeNotifier {
+  String? _token;
+  String? _email;
+  String? _uid;
+  DateTime? _expiryDate;
+
+  bool get isAuth {
+    final isValid = _expiryDate?.isAfter(DateTime.now()) ?? false;
+    return _token != null && isValid;
+  }
+
+  String? get token {
+    return isAuth ? _token : null;
+  }
+
+  String? get email {
+    return isAuth ? _email : null;
+  }
+
+  String? get uid {
+    return isAuth ? _uid : null;
+  }
+
   Future<void> _authenticate(
       String email, String password, String urlFragment) async {
     final _url =
@@ -19,6 +41,14 @@ class Auth with ChangeNotifier {
 
     if (body['error'] != null) {
       throw AuthException(body['error']['message']);
+    } else {
+      _token = body['idToken'];
+      _email = body['email'];
+      _uid = body['localId'];
+
+      _expiryDate =
+          DateTime.now().add(Duration(seconds: int.parse(body['expiresIn'])));
+      notifyListeners();
     }
     print(body);
   }
